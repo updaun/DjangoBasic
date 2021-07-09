e2f8912c72aaa33c1c718dd37498597c458e1343
updaun <updauney@daum.net>
2021-07-09 10:17:45 +0900

    separate rendering http post get method

--- accountapp/models.py
+++ accountapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 # 21.07.09 : add model
 # models.Model 상속받음, django의 기본 모델
-# 명령어 python manage.py makemigrations
-# 명령어 python manage.py migrate
+# 명령어 python manage.py makemigrations  -> 바뀐 부분을 체크
+# 명령어 python manage.py migrate -> 서버에 모델 반영
 class NewModel(models.Model):
     text = models.CharField(max_length=255, null=False)
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -9,4 +9,11 @@ def hello_world(request):
     # HttpResponse ALT+RETURN -> find import
     # 21.07.02
     #return HttpResponse('Hello World!')
-    return render(request, 'accountapp/hello_world.html')
+    # 21.07.09 : rendering 분기
+    #return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text':'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text':'GET METHOD!'})

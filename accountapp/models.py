6191a5c5c4bc0af05018ee96cc0df4e344d003b6
updaun <updauney@daum.net>
2021-07-09 09:34:11 +0900

    add NewModel

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# 21.07.09 : add model
+# models.Model 상속받음, django의 기본 모델
+# 명령어 python manage.py makemigrations
+# 명령어 python manage.py migrate
+class NewModel(models.Model):
+    text = models.CharField(max_length=255, null=False)
+
